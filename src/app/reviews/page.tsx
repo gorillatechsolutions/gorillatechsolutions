2e638a376109350d7df3b71ac28efca772050cc9
P KARMALAKAR <prasenjitkar16@gmail.com>
2025-07-13 18:02:42 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section class="py

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -88,7 +88,7 @@ export default function ReviewsPage() {
       </section>
 
       {/* Reviews Grid */}
-      <section className="py-16 md:py-24">
+      <section className="py-12 md:py-16">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {reviews.map((review, index) => (
