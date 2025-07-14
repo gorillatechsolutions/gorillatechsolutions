2d21f491a0c344bd7ab4c690a31c1d8dae7687df
P KARMALAKAR <prasenjitkar16@gmail.com>
2025-07-14 03:47:53 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="d

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -23,7 +23,7 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
-        xs: "h-7 rounded-md px-2 text-xs",
+        xs: "h-6 rounded-md px-1.5 text-xs",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
@@ -56,6 +56,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
+
 
 
 
