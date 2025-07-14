ba9bfabea503b891784f3f966cf45866cc84e26c
P KARMALAKAR <prasenjitkar16@gmail.com>
2025-07-14 01:01:46 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Card className="m

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -31,7 +31,13 @@ export default function ProfilePage() {
 
     return (
         <div className="container py-12 md:py-20">
-            <Card className="max-w-4xl mx-auto p-6 sm:p-8">
+            <Card className="max-w-4xl mx-auto p-6 sm:p-8 relative">
+                 <div className="absolute top-6 right-6">
+                    <Button variant="outline">
+                        <Edit className="mr-2 h-4 w-4" />
+                        Edit Profile
+                    </Button>
+                </div>
                 <CardContent className="p-0">
                     <div className="flex flex-col sm:flex-row items-start gap-6 sm:gap-8">
                         <Avatar className="h-32 w-32 sm:h-40 sm:w-40 border-4 border-white shadow-md">
@@ -81,13 +87,6 @@ export default function ProfilePage() {
 
                 </CardContent>
             </Card>
-
-             <div className="max-w-4xl mx-auto mt-6 text-right">
-                <Button variant="outline">
-                    <Edit className="mr-2 h-4 w-4" />
-                    Edit Profile
-                </Button>
-            </div>
         </div>
     );
 }
