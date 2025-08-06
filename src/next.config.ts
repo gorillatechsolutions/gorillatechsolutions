c0cc3c8d80fe27b552c39f93bcf083b22014bf32
P KARMALAKAR <prasenjitkar16@gmail.com>
2025-08-06 12:22:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.ts
@@ -0,0 +1,39 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    // This is to allow cross-origin requests in the development environment.
+    // The need for this is specific to the Firebase Studio environment.
+    allowedDevOrigins: [
+      'https://*.cloudworkstations.dev',
+    ],
+    crossOrigin: 'anonymous',
+  },
+};
+
+export default nextConfig;
