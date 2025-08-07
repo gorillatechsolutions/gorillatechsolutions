ea974dd53c9e1aceb8b8aa85cf347f8f01b90f6a
P KARMALAKAR <prasenjitkar16@gmail.com>
2025-08-07 13:01:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- dist/server/webpack-runtime.js
+++ dist/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/async module */
 /******/ 	(() => {
 /******/ 		var webpackQueues = typeof Symbol === "function" ? Symbol("webpack queues") : "__webpack_queues__";
@@ -194,7 +199,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("3a3240743da438a3")
+/******/ 		__webpack_require__.h = () => ("6d3d1f5d8a2263fa")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
